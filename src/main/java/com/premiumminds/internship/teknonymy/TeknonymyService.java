ff5a003aa520c2723d2cd093c3d4661dc7659f32
fc58629 <fc58629@alunos.fc.ul.pt>
2024-05-19 21:25:12 +0100

    Done - David Carreira

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -2,7 +2,9 @@ package com.premiumminds.internship.teknonymy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 class TeknonymyService implements ITeknonymyService {
 
@@ -16,47 +18,117 @@ class TeknonymyService implements ITeknonymyService {
     if (getMaxGenerations(person) == 1) { 
         return ""; // Se a pessoa não tiver filhos, não há tecnônimo
     } else {
-        int maxGeneration = getMaxGenerations(person);
-        Person oldestLastGenerationChild = findOldestLastGenerationChild(person, maxGeneration, 1);
-        return generateTeknonymy(person, oldestLastGenerationChild);
+        List<Person> lastGenerationChildren = new ArrayList<>();
+        //get all last generation children
+        getAllLastGeneration(person, lastGenerationChildren,getMaxGenerations(person),0);
+        //find the oldest child
+        Person oldestChild = findOldestChild(lastGenerationChildren);
+        return generateTeknonymy(person, oldestChild);
     }
-  }
+}
 
+  /**
+   * Method to get the maximum number of generations of a person
+   * @param person
+   * @return int which is the maximum number of generations of a person
+   */
   public static int getMaxGenerations(Person person) {
-    if (person.children() == null || person.children().length == 0) {
-        return 1; // A pessoa é uma folha na árvore
-    }
-    int maxGenerations = 0;
-    for (Person child : person.children()) {
-        maxGenerations = Math.max(maxGenerations, getMaxGenerations(child));
-    }
-    return maxGenerations + 1;
+      if (person.children() == null) {
+          return 1; // A pessoa é uma folha na árvore
+      }
+      int maxGenerations = 0;
+      //get the maximum number of generations of each child
+      for (Person child : person.children()) {
+          maxGenerations = Math.max(maxGenerations, getMaxGenerations(child));
+      }
+      return maxGenerations + 1;
   }
 
-  private Person findOldestLastGenerationChild(Person person, int maxGeneration, int currentGeneration) {
-    if (person.children() == null || person.children().length == 0) {
-        return person;
-    }
 
-    Person oldestChild = null;
-    for (Person child : person.children()) {
-      if (currentGeneration == maxGeneration - 1) {
-        if (oldestChild == null || child.dateOfBirth().isBefore(oldestChild.dateOfBirth())) {
-          oldestChild = child;
+  /**
+   * Method to get all the last generation children of a person
+   * @param person
+   * @param lastGenerationChildren
+   * @param maxGeneration
+   * @param currentGeneration
+   */
+  private void getAllLastGeneration(Person person, List<Person> lastGenerationChildren, int maxGeneration, int currentGeneration) {
+    //if person is last generation child add to list
+    if (currentGeneration == maxGeneration - 1 && (person.children() == null)){
+        lastGenerationChildren.add(person);
+    } else if (person.children() != null) {
+        //if person has children, get all last generation children of each child
+        for (Person child : person.children()) {
+            getAllLastGeneration(child, lastGenerationChildren, maxGeneration, currentGeneration + 1);
         }
-      } else {
-        Person candidate = findOldestLastGenerationChild(child, maxGeneration, currentGeneration + 1);
-        if (oldestChild == null || candidate.dateOfBirth().isBefore(oldestChild.dateOfBirth())) {
-          oldestChild = candidate;
-        }
+    }
+  }
+
+
+  /**
+   * Method to find the oldest child of a list of children
+   * @param children
+   * @return Person which is the oldest child
+   */
+  private Person findOldestChild(List<Person> children) {
+      //if there are no children return null to prevent null pointer exception
+      if (children.isEmpty()) {
+          return null;
+      }
+      Person oldestChild = children.get(0);
+      //find the oldest child by comparing the date of birth
+      for (Person child : children) {
+          if (child.dateOfBirth().isBefore(oldestChild.dateOfBirth())) {
+            oldestChild = child;
+          }
+      }
+      return oldestChild;
+  }
+
+
+  /**
+   * Method to generate the Teknonymy Name of a person
+   * @param person
+   * @param lastGenerationChild
+   * @return String which is the Teknonymy Name
+   */
+  private String generateTeknonymy(Person person, Person lastGenerationChild) {
+      int generation = getMaxGenerations(person);
+      String role = "";
+      if (person.sex() == 'M'){
+          if (generation == 2){
+            role = "father";
+          }
+          else if (generation == 3){
+            role = "grandfather";
+          }
+          else if (generation == 4){
+            role = "great-grandfather";
+          }
+          else if (generation == 5){
+            role = "great-great-grandfather";
+          }
+          else if (generation > 5){
+            role = "ancestor";
+          }
+          return role + " of " + lastGenerationChild.name();
+      }
+      else {
+          if (generation == 2){
+            role = "mother";
+          }
+          else if (generation == 3){
+            role = "grandmother";
+          }
+          else if (generation == 4){
+            role = "great-grandmother";
+          }
+          else if (generation == 5){
+            role = "great-great-grandmother";
+          }else if (generation > 5){
+            role = "ancestor";
+          }
+          return role + " of " + lastGenerationChild.name();
       }
     }
-
-    return oldestChild;
-  }
-
-  private String generateTeknonymy(Person person, Person lastGenerationChild) {
-    String role = person.sex() == 'M' ? "father" : "mother";
-    return role + " of " + lastGenerationChild.name();
-  }
 }
--- src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
+++ src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
@@ -58,7 +58,7 @@ public class TeknonymyServiceTest {
         new Person[]{ new Person("Holy",'F', new Person[]{ new Person("Mary",'F', null, LocalDateTime.of(1046, 1, 1, 0, 0)) }, LocalDateTime.of(1046, 1, 1, 0, 0)) },
         LocalDateTime.of(1046, 1, 1, 0, 0));
     String result = new TeknonymyService().getTeknonymy(person);
-    String expected = "father of Mary";
+    String expected = "grandfather of Mary";
     assertEquals(result, expected);
   }
 
@@ -70,7 +70,7 @@ public class TeknonymyServiceTest {
         new Person[]{ new Person("Holy",'F', new Person[]{ new Person("Mary",'F', new Person[]{ new Person("Jane",'F', null, LocalDateTime.of(1046, 1, 1, 0, 0)) }, LocalDateTime.of(1046, 1, 1, 0, 0)) }, LocalDateTime.of(1046, 1, 1, 0, 0)) },
         LocalDateTime.of(1046, 1, 1, 0, 0));
     String result = new TeknonymyService().getTeknonymy(person);
-    String expected = "father of Jane";
+    String expected = "great-grandfather of Jane";
     assertEquals(result, expected);
   }
 
@@ -85,12 +85,12 @@ public class TeknonymyServiceTest {
     Person D = new Person("D",'F',new Person[]{S,E}, LocalDateTime.of(2003,1,1,0,0));
 
     String result = new TeknonymyService().getTeknonymy(D);
-    String expected = "mother of H";
+    String expected = "grandmother of H";
     assertEquals(result, expected);
   }
 
   @Test
-  public void GivenTree(){
+  public void GivenTreeTest1(){
     Person F = new Person("F", 'M', null, LocalDateTime.of(2018,1,1,0,0));
     Person G = new Person("G", 'M', null, LocalDateTime.of(2021,1,1,0,0));
     Person H = new Person("H", 'M', null, LocalDateTime.of(2022,1,1,0,0));
@@ -104,7 +104,106 @@ public class TeknonymyServiceTest {
     Person A = new Person("A",'M',new Person[]{B,C,D}, LocalDateTime.of(1990,1,1,0,0));
 
     String result = new TeknonymyService().getTeknonymy(A);
-    String expected = "father of H";
+    String expected = "grandfather of F";
     assertEquals(result, expected);
   }
+
+@Test
+public void GivenTreeTest2(){
+  Person F = new Person("F", 'M', null, LocalDateTime.of(2018,1,1,0,0));
+  Person G = new Person("G", 'M', null, LocalDateTime.of(2021,1,1,0,0));
+  Person H = new Person("H", 'M', null, LocalDateTime.of(2022,1,1,0,0));
+
+  Person E = new Person("E",'M',null, LocalDateTime.of(2019,1,1,0,0));
+
+  Person D = new Person("D",'F',new Person[]{F,G,H}, LocalDateTime.of(2003,1,1,0,0));
+  Person C = new Person("C",'M',null, LocalDateTime.of(2002,1,1,0,0));
+  Person B = new Person("B",'M',new Person[]{E}, LocalDateTime.of(2000,1,1,0,0));
+
+  Person A = new Person("A",'M',new Person[]{B,C,D}, LocalDateTime.of(1990,1,1,0,0));
+
+  String result = new TeknonymyService().getTeknonymy(B);
+  String expected = "father of E";
+  assertEquals(result, expected);
+}
+
+@Test
+public void GivenTreeTest3(){
+  Person F = new Person("F", 'M', null, LocalDateTime.of(2018,1,1,0,0));
+  Person G = new Person("G", 'M', null, LocalDateTime.of(2021,1,1,0,0));
+  Person H = new Person("H", 'M', null, LocalDateTime.of(2022,1,1,0,0));
+
+  Person E = new Person("E",'M',null, LocalDateTime.of(2019,1,1,0,0));
+
+  Person D = new Person("D",'F',new Person[]{F,G,H}, LocalDateTime.of(2003,1,1,0,0));
+  Person C = new Person("C",'M',null, LocalDateTime.of(2002,1,1,0,0));
+  Person B = new Person("B",'M',new Person[]{E}, LocalDateTime.of(2000,1,1,0,0));
+
+  Person A = new Person("A",'M',new Person[]{B,C,D}, LocalDateTime.of(1990,1,1,0,0));
+
+  String result = new TeknonymyService().getTeknonymy(D);
+  String expected = "mother of F";
+  assertEquals(result, expected);
+}
+
+@Test
+public void GivenTreeTest4(){
+    Person F = new Person("F", 'M', null, LocalDateTime.of(2018,1,1,0,0));
+    Person G = new Person("G", 'M', null, LocalDateTime.of(2021,1,1,0,0));
+    Person H = new Person("H", 'M', null, LocalDateTime.of(2022,1,1,0,0));
+
+    Person E = new Person("E",'M',null, LocalDateTime.of(2019,1,1,0,0));
+
+    Person D = new Person("D",'F',new Person[]{F,G,H}, LocalDateTime.of(2003,1,1,0,0));
+    Person C = new Person("C",'M',null, LocalDateTime.of(2002,1,1,0,0));
+    Person B = new Person("B",'M',new Person[]{E}, LocalDateTime.of(2000,1,1,0,0));
+
+    Person A = new Person("A",'M',new Person[]{B,C,D}, LocalDateTime.of(1990,1,1,0,0));
+
+    String result = new TeknonymyService().getTeknonymy(C);
+    String expected = "";
+    assertEquals(result, expected);
+}
+
+
+@Test
+public void givenTreeTestWithMultipleGenerations() {
+        Person J = new Person("J", 'M', null, LocalDateTime.of(2025, 1, 1, 0, 0));
+        Person K = new Person("K", 'F', null, LocalDateTime.of(2026, 1, 1, 0, 0));
+        
+        Person F = new Person("F", 'M', null, LocalDateTime.of(2018, 1, 1, 0, 0));
+        Person G = new Person("G", 'M', null, LocalDateTime.of(2021, 1, 1, 0, 0));
+        Person H = new Person("H", 'M', null, LocalDateTime.of(2022, 1, 1, 0, 0));
+        Person I = new Person("I", 'F', new Person[]{J, K}, LocalDateTime.of(2015, 1, 1, 0, 0));
+        
+        Person E = new Person("E", 'M', null, LocalDateTime.of(2019, 1, 1, 0, 0));
+        Person D = new Person("D", 'F', new Person[]{F, G, H, I}, LocalDateTime.of(2003, 1, 1, 0, 0));
+        Person C = new Person("C", 'M', null, LocalDateTime.of(2002, 1, 1, 0, 0));
+        Person B = new Person("B", 'M', new Person[]{E}, LocalDateTime.of(2000, 1, 1, 0, 0));
+
+        Person A = new Person("A", 'M', new Person[]{B, C, D}, LocalDateTime.of(1990, 1, 1, 0, 0));
+        
+        // Serviço
+        TeknonymyService service = new TeknonymyService();
+        
+        // Verify for A
+        String resultA = service.getTeknonymy(A);
+        String expectedA = "great-grandfather of J";
+        assertEquals(expectedA, resultA);
+        
+        // Verify for D
+        String resultD = service.getTeknonymy(D);
+        String expectedD = "grandmother of J";
+        assertEquals(expectedD, resultD);
+        
+        // Verify for C
+        String resultC = service.getTeknonymy(C);
+        String expectedC = "";
+        assertEquals(expectedC, resultC);
+        
+        // Verifify for B
+        String resultB = service.getTeknonymy(B);
+        String expectedB = "father of E";
+        assertEquals(expectedB, resultB);
+    }
 }
